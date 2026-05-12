f9dc00d1be154eb456073b5409f0a17b424401ab
Yun <kimjinyeon16@gmail.com>
2026-05-12 11:25:22 -0500

    Actualizar sistema de notas v2

---
+++ data/alumnos.js
@@ -0,0 +1,18 @@
+const alumnos =[
+    {
+        id: 1,
+        nombre: "Maria",
+        nota1: 15,
+        nota2: 18,
+        nota3: 20
+    },
+    {
+        id: 2,
+        nombre: "Darien",
+        nota1: 10,
+        nota2: 12,
+        nota3: 18
+    }
+]
+
+module.exports = alumnos;
